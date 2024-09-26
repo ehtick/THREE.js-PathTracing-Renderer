9761ed861901f9b7eba431868f4103b097ce1d97
Erich Loftis <erichlof@swbell.net>
2024-09-26 08:59:48 -0500

    Update Grid_Acceleration_Fragment.glsl

--- shaders/Grid_Acceleration_Fragment.glsl
+++ shaders/Grid_Acceleration_Fragment.glsl
@@ -345,14 +345,7 @@ void main(void)
                 previousColor *= 0.8; // motion-blur trail amount (old image)
                 pixelColor *= 0.2; // brightness of new image (noisy)
         }
-
-	// if current raytraced pixel didn't return any color value, just use the previous frame's pixel color
-	if (pixelColor == vec3(0.0))
-	{
-		pixelColor = previousColor;
-		previousColor *= 0.5;
-		pixelColor *= 0.5;
-	}
+	
 
 	pc_fragColor = vec4(pixelColor + previousColor, 1.01);
 }
