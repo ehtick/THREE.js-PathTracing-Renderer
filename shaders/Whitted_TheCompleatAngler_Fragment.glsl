368754779ee446a536599c526dbb183a34e5beb7
Erich Loftis <erichlof@swbell.net>
2024-09-26 09:05:22 -0500

    Update Whitted_TheCompleatAngler_Fragment.glsl

--- shaders/Whitted_TheCompleatAngler_Fragment.glsl
+++ shaders/Whitted_TheCompleatAngler_Fragment.glsl
@@ -507,14 +507,7 @@ void main( void )
                 previousColor *= 0.6; // motion-blur trail amount (old image)
                 pixelColor *= 0.4; // brightness of new image (noisy)
         }
-
-	// if current raytraced pixel didn't return any color value, just use the previous frame's pixel color
-	/* if (pixelColor == vec3(0.0))
-	{
-		pixelColor = previousColor;
-		previousColor *= 0.5;
-		pixelColor *= 0.5;
-	} */
+	
         
         pc_fragColor = vec4( pixelColor + previousColor, 1.01);		
 }
