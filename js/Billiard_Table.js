892f55ca02bd1a342ba2a4dd4ac22b9ddea7eeea
Erich Loftis <erichlof@swbell.net>
2024-11-24 23:28:59 -0600

    Update Billiard_Table.js

--- js/Billiard_Table.js
+++ js/Billiard_Table.js
@@ -14,13 +14,10 @@ function initSceneData()
 	// scene/demo-specific three.js objects setup goes here
 	sceneIsDynamic = false;
 
-	edgeSharpenSpeed = 0.1;
-	filterDecaySpeed = 0.005;
-
 	cameraFlightSpeed = 100;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 0.75 : 0.75;
+	pixelRatio = mouseControl ? 1.0 : 0.75;
 
 	EPS_intersect = 0.01;
 
