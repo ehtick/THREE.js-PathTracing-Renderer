237f649d81461ea4e6b1ee37f0805818d330ffd5
Erich Loftis <erichlof@swbell.net>
2024-11-24 23:20:05 -0600

    Update Appel_ShadingMachineRenderingsOfSolids.js

--- js/Appel_ShadingMachineRenderingsOfSolids.js
+++ js/Appel_ShadingMachineRenderingsOfSolids.js
@@ -707,7 +707,7 @@ function initSceneData()
 	mobileUseDarkButtons = true;
 
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 1.0 : 1.0;
+	pixelRatio = mouseControl ? 1.0 : 1.0; // mobile devices can also handle full resolution for this demo
 
 	EPS_intersect = 0.01;
 
